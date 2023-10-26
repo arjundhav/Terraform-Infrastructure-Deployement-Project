0e054d5b2c8e0664b94143f4ecd0effed2423a06
Arjun Jadhav <63765264+arjundhav@users.noreply.github.com>
2023-10-26 22:06:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,8 @@ resource "aws_instance" "myFirstInstance" {
 resource "aws_eip" "myFirstInstance" {
   vpc      = true
   instance = aws_instance.myFirstInstance.id
-tags= {
+  tags = {
     Name = "my_elastic_ip"
   }
 }
+
